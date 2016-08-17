fbac6be464a49aba9285278ccfd6f27889ae29d0
bgthomasson <benthomonster@gmail.com>
2016-08-17 16:36:47 -0500

    ok maybe it won't be easy

--- minzip.py
+++ minzip.py
@@ -1,65 +1,68 @@
-# CSS MINZIP
-# a tool to minimize and gzip CSS files
-# step 1. Open filename.css and copy the contents
+# CSS MINZIP 
+# Quick and dirty tool to minimize (and gzip??) CSS files 
+# step 1. Open file and copy the contents
 # [can we check to see if it's in an unsaved state!?]
-# step 2. Remove all whitespace  
+# step 2. Remove all whitespace 
 # step 3. join() with no spaces in between
 # step 4. remove comments, leaving no spaces
-# step 5. GZIP!?
+# step 5. GZIP!? I tihnk this would only be easy on *nix. 
 # step 6. Write minimized-zipped code to filename.css.zip
+# option: compare filesize of source to final, X kb vs Y kb
 # -----------------------------------------------------------------------------
-
-# maybe best to put all imports first C style
+# maybe best to put all imports first 
 
 import re, sys
 
 def bassomatic(): 
     # Not sure best way to input so I'll default to CLI style for testing.
-    # Use sample.css for your convenience
+    print("(Use sample.css for testing purposes)")
     filename = input("Enter a file name to make a minimized copy: ")
 
     # We need to pull out the guts- err, content.
     raw = open(filename, 'r')
 
     # Then chop it up along newlines, and split() removes the whitespace.
+    # So this is absurdly easy, while the regex is absurdly hard.
     diced = raw.read().split()
 
-    # now we jam it all back together so we can use regex on it
+    # now we jam it all back together 
     spliced = "".join(diced)
 
     return spliced
 
-# OF COURSE I'm having problems with getting it to work
-# Even putting the "r" in front isn't escaping the *
-def rex(spliced):
 
-    # pattern = r'/\* [.]+ \*/'
-    # I'm not understandin the logic here, whatever it may be, so a test-
+def rex(diced):
 
-    pattern = r'regex'
+    pattern = r'/\*(.+)\*/'
+    # OK learned 2 things here, first, don't leave spaces in regex.
+    # second, it skips all the way to the very last */ in the string.
+    # Thus, -everything- in the file is selected since I have a comment
+    # at the end... oops. At least I got the regex right.
 
     regex = re.compile(pattern)
 
-    final_output = re.sub(regex, 'VOODOO', spliced)
+    commentless_output = re.sub(regex, '', diced)
 
-    return final_output
+    return commentless_output
 
 
 def write_to_file(final_output): 
-    # IF (filename exists) prompt to rename OR overwrite ELSE-
+    # IF (filename exists) prompt to save a copy like file2 OR overwrite
+    # ELSE-
     output = open("filename.min.css.zip", "w")
     output.writelines(final_output)
     output.close()
     # success/fail dialogue 
 
-def write_to_cli(final_output):
-    print(final_output)
+
+def write_to_cli(output):
+    print(output)
 
 
 def main():
-    spliced = bassomatic()
-    final_output = rex(spliced)
+    final_output = bassomatic()
     write_to_cli(final_output)
+
 
 # With everything encapsulated and digestable we just run 
 main()
