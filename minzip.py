ba4d0b7629dabfd52b3a7963799d38ebe9325a0b
bgthomasson <benthomonster@gmail.com>
2016-08-16 20:11:03 -0500

    encapsulated into functions

--- minzip.py
+++ minzip.py
@@ -4,45 +4,62 @@
 # [can we check to see if it's in an unsaved state!?]
 # step 2. Remove all whitespace  
 # step 3. join() with no spaces in between
-# step 4. run regex pattern to remove comments
+# step 4. remove comments, leaving no spaces
 # step 5. GZIP!?
 # step 6. Write minimized-zipped code to filename.css.zip
-# [check to see if that filename already exists]
+# -----------------------------------------------------------------------------
 
-import re
+# maybe best to put all imports first C style
 
-# Not sure best way to input so I'll default to CLI style for testing.
-# Use sample.css for your convenience
-filename = input("Enter a file name to make a minimized copy: ")
+import re, sys
 
-# We need to pull out the guts- err, content.
-raw = open(filename, 'r')
+def bassomatic(): 
+    # Not sure best way to input so I'll default to CLI style for testing.
+    # Use sample.css for your convenience
+    filename = input("Enter a file name to make a minimized copy: ")
 
-# Then chop it up along newlines, and split() removes the whitespace easily.
-diced = raw.read().split()
+    # We need to pull out the guts- err, content.
+    raw = open(filename, 'r')
 
-# now we jam it all back together so we can use regex on it
-spliced = "".join(diced)
+    # Then chop it up along newlines, and split() removes the whitespace.
+    diced = raw.read().split()
+
+    # now we jam it all back together so we can use regex on it
+    spliced = "".join(diced)
+
+    return spliced
 
 # OF COURSE I'm having problems with getting it to work
 # Even putting the "r" in front isn't escaping the *
-# Also I can't find a pattern that works just reading basic how-to stuff
-# Problem is we have /* and */ as delimiters, with a variable amount between. 
+def rex(spliced):
 
-pattern = r'/\* [.]+ \*/'
+    # pattern = r'/\* [.]+ \*/'
+    # I'm not understandin the logic here, whatever it may be, so a test-
 
-regex = re.compile(pattern)
+    pattern = r'regex'
 
-final = re.sub(regex, '', spliced)
+    regex = re.compile(pattern)
 
-# GZIP!?
+    final_output = re.sub(regex, 'VOODOO', spliced)
 
-# IF (filename exists) prompt to rename OR overwrite ELSE-
-# output = open("filename.min.css.zip", "w")
-# output.writelines(spliced)
-# output.close()
-# success/fail dialogue 
+    return final_output
 
-# here is our test output until then: 
-print(spliced)
-print(final) 
+
+def write_to_file(final_output): 
+    # IF (filename exists) prompt to rename OR overwrite ELSE-
+    output = open("filename.min.css.zip", "w")
+    output.writelines(final_output)
+    output.close()
+    # success/fail dialogue 
+
+def write_to_cli(final_output):
+    print(final_output)
+
+
+def main():
+    spliced = bassomatic()
+    final_output = rex(spliced)
+    write_to_cli(final_output)
+
+# With everything encapsulated and digestable we just run 
+main()
