911246f50a359bdd480719e79bc96e888440a1fd
bgthomasson <benthomonster@gmail.com>
2017-05-19 18:34:46 -0500

    edits

--- filefinder.py
+++ filefinder.py
@@ -4,16 +4,12 @@
 GOALS:
 I'm trying to standardize my workflow: all writing should be in ODT,
 so any RTF or DOC files should be saved as such, and the original trashed.
-You can do this in BASH or GUI but I thought this was good practice.
 
 #TODO: allow choice of dir to search
-#TODO: choose filetype to find (dropmenu?)
 #bonus: check if RTF files have eponymous ODT/DOC/etc versions already
-#maybe: for TXT files, check if UTF-8 (st_mode?)
->> would require flexible interface
 #TODO: choose program to open files with. import subprocess
-?: subprocess.run(['usr/bin/libreoffice', url])
-?: or libreoffice --writer %U
+?: subprocess.run(['/usr/bin/libreoffice', url])
+?: CLI: /usr/bin/libreoffice -o url
 #TODO: preview contents
 # DELETE os.remove(url)
 
@@ -23,6 +19,7 @@ import os
 from time import localtime, strftime
 from tkinter import *
 from tkinter import ttk
+import subprocess
 
 # global vars (oh noes)
 userhome = os.environ['HOME']
@@ -52,28 +49,39 @@ def finder():
 
 
 
+def open_in_editor(url):
+    subprocess.run(['/usr/bin/libreoffice', url])
 
-'''
-def previewer():
-try:
-    fp = open("myfile")
-except PermissionError:
-    return "I kinna do that cap'n!"
-else:
-    with fp:
-        return fp.read()
-'''
+    
+
+def nukem(url):
+    try:
+        os.remove(url)
+    except:
+        return "Where is it?"
+            
+
+
+def previewer(url):
+    try:
+        fp = open("url")
+    except PermissionError:
+        return "I kinna do that cap'n!"
+    else:
+        with fp:
+            return fp.read()
+
 
 
 # GUI -------------------------------------------------------------------
 
 root = Tk()
 root.title("File Ferret")
+# root.option_add('*tearOff', FALSE)
 
 self = ttk.Frame(root, padding="3 3 12 12")
-
 self.grid(column=0, row=0, sticky=(N, S, E, W))
-
+# make it resizable- I know switch from root to self is weird but it works 
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 self.rowconfigure(1, weight=3)
@@ -89,7 +97,7 @@ home_label.grid(row=0, column=0, sticky=W)
 
 filetypes = [ 'rtf', 'txt', 'doc' ]
 filetype = StringVar()
-# TypeError: endswith first arg must be str or a tuple of str, not StringVar
+# some bullshit about not passing variables directly so get/set
 
 opt_rtf = ttk.Radiobutton(self, text='RTF', variable=filetype, value='rtf')
 opt_rtf.grid(row=0, column=1)
@@ -102,18 +110,25 @@ search_button = ttk.Button(self, text="Search", command=finder)
 search_button.grid(row=0, column=5, sticky=E)
 filetype.set('')
 
+sep = ttk.Separator(root, orient=HORIZONTAL) 
+sep.grid(row=1, columnspan=5, sticky=EW)
+
 # menu header
-filename_label = ttk.Label(self, text="Filename")
+filename_label = ttk.Label(self, text="Filename") 
 filesize_label = ttk.Label(self, text="Size")
 edited_label = ttk.Label(self, text="Edited")
 preview_panel = ttk.Label(self, text="Preview")
 
-filename_label.grid(row=1, column=0)
-filesize_label.grid(row=1, column=1)
-edited_label.grid(row=1, column=2)
-preview_panel.grid(row=1, column=3, columnspan=2, sticky=E)
+filename_label.grid(row=2, column=0)
+filesize_label.grid(row=2, column=1)
+edited_label.grid(row=2, column=2)
+preview_panel.grid(row=2, column=3, columnspan=2, sticky=E)
 
+# text = Text(self, width=30, height=100)
+# use this as preview pane??
 
+# $filename = ttk.Button(self, text=filename, command=open_in_editor)
+# $filename.grid(row=3+i, column=0)
 
 for child in self.winfo_children(): child.grid_configure(padx=5, pady=5)
 
