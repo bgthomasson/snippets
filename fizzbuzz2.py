d55442600d608611e25e56f7e3b25559bcfb3e24
bgthomasson <benthomonster@gmail.com>
2017-09-14 12:21:24 -0500

    added comments

--- fizzbuzz2.py
+++ fizzbuzz2.py
@@ -1,15 +1,19 @@
 #! python3
+# this has no need to be interactive except to grab attention
+# I have no current reason to do these as functions except 80 col limit
 
-def start():
+def starter():
     start_val = int(input('Enter an integer for the starting value: '))
     return start_val
 
-def end():
+def ender():
     end_val = int(input('Enter an integer for the ending value: ')) + 1
     return end_val
 
-def fizzbuzz(starter, ender):
-    for x in range(starter, ender):
+# range is exclusive, so adding 1 to the end value is a bodge to dodge this
+
+def fizzbuzz(start_val, end_val):
+    for x in range(start_val, end_val):
         if x % 3 == 0 and x % 5 == 0:
             print('FIZZBUZZ!')
         elif x % 3 == 0:
@@ -26,9 +30,9 @@ def main():
         print("FIZZBUZZ 2")
 
         try: 
-            starter = start()
-            ender = end()
-            fizzbuzz(starter, ender)
+            start_val = starter()
+            end_val = ender()
+            fizzbuzz(start_val, end_val)
             print("Want to keep going? Y to continue, any other key to quit:")
             rerun = input()
             
